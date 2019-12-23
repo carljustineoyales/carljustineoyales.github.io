c0c2c9f897bc5ec4e179e285111d02ca56e0582b
carl justine oyales <51884760+carljustineoyales@users.noreply.github.com>
2019-12-23 08:49:33 +0800

    Updates

---
+++ precache-manifest.81cd986c02f00b89944a807ad3604828.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb4b7444391aaa2cd2fb5684f2a3e8e0",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "70cf47f01d257f54c6ff",
+    "url": "/test/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3a4b67294a7cf3f9aca1",
+    "url": "/test/static/css/main.ea487407.chunk.css"
+  },
+  {
+    "revision": "70cf47f01d257f54c6ff",
+    "url": "/test/static/js/2.4beee3f7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test/static/js/2.4beee3f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a4b67294a7cf3f9aca1",
+    "url": "/test/static/js/main.e921aaec.chunk.js"
+  },
+  {
+    "revision": "2fc2f2f7dde7d538ecf3",
+    "url": "/test/static/js/runtime-main.4cd6fb1f.js"
+  }
+]);
